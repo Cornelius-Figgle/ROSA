609da6ff8bdd6113822c8aae579942b655b4660d
Max <max@fullimage.net>
2022-08-19 18:08:57 +0100

    redid code from site

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+#!T:\projects\ROSA\rosa-env\Scripts\python.exe
+
+#https://www.thepythoncode.com/article/using-speech-recognition-to-convert-speech-to-text-python
+
 from time import sleep
 
 import speech_recognition as sr
@@ -15,31 +19,15 @@ def startup():
     print('\n\t\tADJUSTING FOR AMIENBT')
     with m as source: r.adjust_for_ambient_noise(source) # we only need to calibrate once, before we start listening
 
-def backgroundListening():
-    # initialize the recognizer
-    # this is called from the background thread
-    def callback(recog, audio):
-        try: 
-            speech = recog.recognize_google(audio).upper()
-            print(f'\n\t{speech}')
-            stop_listening(wait_for_stop=False)
-            print('\n\tEND OF REC')
-        except: 
-            print('\n\t\tError in recording, ended')
-            stop_listening(wait_for_stop=False)
+def main():
+    with sr.Microphone() as source:
+        # read the audio data from the default microphone
+        audio_data = r.record(source, duration=5)
+        print("Recognizing...")
+        # convert speech to text
+        text = r.recognize_google(audio_data)
+        print(text)
 
-    print('\n\t\tLISTENING')
-    # start listening in the background (note that we don't have to do this inside a `with` statement)
-    stop_listening = r.listen_in_background(m, callback)
-    # `stop_listening` is now a function that, when called, stops background listening
-
-    # do some unrelated computations for 5 seconds
-    for i in range(0, 20): sleep(0.1)  # we're still listening even though the main thread is doing other things
-
-    # calling this function requests that the background listener stop listening
-    #stop_listening(wait_for_stop=False)
-
-def main(): backgroundListening()
 
 
 #________________________________________________________________________________________________________________________________
