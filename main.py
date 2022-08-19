6eaf13db66b93642c0c16e5b9fa568b1ee321e00
Max <max@fullimage.net>
2022-08-19 19:14:20 +0100

    setup bg listening

--- main.py
+++ main.py
@@ -15,40 +15,27 @@ def startup():
     with sr.Microphone() as source: 
         sr.Recognizer().adjust_for_ambient_noise(source) # we only need to calibrate once, before we start listening
 
-def backgroundKeywords():
+def backgroundListening():
     # obtain audio from the microphone
     r = sr.Recognizer()
     with sr.Microphone() as source:
         print('Say something!')
         audio = r.listen(source)
 
-    # recognize speech using Google Speech Recognition
     try:
-        # for testing purposes, we're just using the default API key to use another API key, use `r.recognize_google(audio, key='GOOGLE_SPEECH_RECOGNITION_API_KEY')` instead of `r.recognize_google(audio)`
-        print('Google Speech Recognition thinks you said \'' + r.recognize_google(audio) + '\'')
+        speech = r.recognize_google(audio).lower()
+        if 'rosa' in speech:
+            answer(speech)
     except sr.UnknownValueError:
         print('Google Speech Recognition could not understand audio')
     except sr.RequestError as e:
         print('Could not request results from Google Speech Recognition service; {0}'.format(e))
 
-def queryListen():
-    # obtain audio from the microphone
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
-        print('Say something!')
-        audio = r.listen(source)
-
-    # recognize speech using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key to use another API key, use `r.recognize_google(audio, key='GOOGLE_SPEECH_RECOGNITION_API_KEY')` instead of `r.recognize_google(audio)`
-        print('Google Speech Recognition thinks you said \'' + r.recognize_google(audio) + '\'')
-    except sr.UnknownValueError:
-        print('Google Speech Recognition could not understand audio')
-    except sr.RequestError as e:
-        print('Could not request results from Google Speech Recognition service; {0}'.format(e))
+def answer(query):
+    pass
 
 def main():
-    queryListen()
+    backgroundListening()
 
 #________________________________________________________________________________________________________________________________
 
