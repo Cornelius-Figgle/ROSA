168c5d9661e2b454fe481eb58200d908fed9c203
Max <max@fullimage.net>
2022-08-19 18:18:56 +0100

    code from module docs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!T:\projects\ROSA\rosa-env\Scripts\python.exe
 
+#https://github.com/Cornelius-Figgle/ROSA
 #https://www.thepythoncode.com/article/using-speech-recognition-to-convert-speech-to-text-python
 
 from time import sleep
@@ -9,24 +10,26 @@ from playsound import playsound
 
 #________________________________________________________________________________________________________________________________
 
-activationPhrase = ['ROSA'] #etc
-r = sr.Recognizer()
-m = sr.Microphone()
-
-#________________________________________________________________________________________________________________________________
-
 def startup():
-    print('\n\t\tADJUSTING FOR AMIENBT')
-    with m as source: r.adjust_for_ambient_noise(source) # we only need to calibrate once, before we start listening
+    print('ADJUSTING FOR AMIENBT')
+    with sr.Microphone() as source: 
+        sr.Recognizer().adjust_for_ambient_noise(source) # we only need to calibrate once, before we start listening
 
 def main():
+    # obtain audio from the microphone
+    r = sr.Recognizer()
     with sr.Microphone() as source:
-        # read the audio data from the default microphone
-        audio_data = r.record(source, duration=5)
-        print("Recognizing...")
-        # convert speech to text
-        text = r.recognize_google(audio_data)
-        print(text)
+        print('Say something!')
+        audio = r.listen(source)
+
+    # recognize speech using Google Speech Recognition
+    try:
+        # for testing purposes, we're just using the default API key to use another API key, use `r.recognize_google(audio, key='GOOGLE_SPEECH_RECOGNITION_API_KEY')` instead of `r.recognize_google(audio)`
+        print('Google Speech Recognition thinks you said ' + r.recognize_google(audio))
+    except sr.UnknownValueError:
+        print('Google Speech Recognition could not understand audio')
+    except sr.RequestError as e:
+        print('Could not request results from Google Speech Recognition service; {0}'.format(e))
 
 
 
