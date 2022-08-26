6280c1ee4591c63cef16d9da2e4632d6b6a01a4d
Max <max@fullimage.net>
2022-08-26 19:39:44 +0100

    PyQt5 setup

--- rosa_gui_installer.py
+++ rosa_gui_installer.py
@@ -1,6 +1,21 @@
 import os
 
+import PyQt5.QtWidgets as qt
+
+#________________________________________________________________________________________________________________________________
+
+app = qt.QApplication([])
+
+#________________________________________________________________________________________________________________________________
+
 def main():
     print(os.name)
+    window = qt.QWidget()
+    layout = qt.QVBoxLayout()
+    layout.addWidget(qt.QPushButton('Top'))
+    layout.addWidget(qt.QPushButton('Bottom'))
+    window.setLayout(layout)
+    window.show()
+    app.exec()
 
 if __name__ == '__main__': main()
