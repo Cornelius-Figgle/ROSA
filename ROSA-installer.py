e623e9aa12711ea5b6fefe8322e0ccb845a52fe9
Cornelius-Figgle <max@fullimage.net>
2022-09-23 19:14:41 +0100

    hm thread changes

--- ROSA-installer.py
+++ ROSA-installer.py
@@ -226,8 +226,12 @@ class installROSA(qt.QWizardPage):
 	def initializePage(self) -> None:
 		print(installConfigs)
 
-		fred = Thread(target=self.threadProcesses, daemon=True)
+		fred = Thread(target=self.threadProcesses)
 		fred.start()
+
+		fred.join()
+		print('install complete!')
+		self.infoLabel.setText('install complete!')
 
 	def threadProcesses(self) -> None:
 		if os.name == 'nt':
@@ -258,9 +262,6 @@ class installROSA(qt.QWizardPage):
 			self.make_shortcut(self.downloadedFiles['config'], installConfigs['startPath'])
 			self.make_shortcut(self.downloadedFiles['readme'], installConfigs['startPath'])
 			self.make_shortcut(self.downloadedFiles['bin'], installConfigs['deskPath'])		
-
-		print('install complete!')
-		self.infoLabel.setText('install complete!')
 
 	def download_file(self, url) -> str:
 		local_dirname = os.path.join(os.path.dirname(__file__), 'temp_dwld')
