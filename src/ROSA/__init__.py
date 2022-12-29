4bb01ec592bf4c4209e484fea0ef41fe81f7c929
Cornelius-Figgle <max@fullimage.net>
2022-12-29 09:27:08 +0000

    added credits sec

---
+++ src/ROSA/__init__.py
@@ -0,0 +1,32 @@
+# 
+# 
+# -*- coding: UTF-8 -*-
+
+# https://github.com/Cornelius-Figgle/ROSA/
+# ROBOTICALLY OBNOXIOUS SERVING ASSISTANT
+
+'''
+THIS FILE IS PART OF THE `ROSA` REPO, MAINTAINED AND PRODUCED BY MAX 
+HARRISON, AS OF 2022
+
+It may work separately and independently of the main repo, it may not
+
+- Code (c) Max Harrison 2022
+- Ideas (c) Callum Blumfield 2022
+- Ideas (c) Max Harrison 2022
+- Vocals (c) Evie Peacock 2022
+
+Thanks also to Alex, Ashe & Jake for support throughout (sorry for the
+spam). also thanks to all the internet peoples that helped with this
+as well 
+'''
+
+# note: view associated GitHub info as well
+__version__ = 'Pre-release'  
+__author__ = 'Cornelius-Figgle'
+__email__ = 'max@fullimage.net'
+__maintainer__ = 'Cornelius-Figgle'
+__copyright__ = 'Copyright (c) 2022 Max Harrison'
+__license__ = 'MIT'
+__status__ = 'Development'
+__credits__ = ['Max Harrison', 'Callum Blumfield', 'Evie Peacock']
