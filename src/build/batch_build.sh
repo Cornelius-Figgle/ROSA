6f7715a863066b6de448f757a23148caec38703e
Cornelius-Figgle <max@fullimage.net>
2023-02-17 08:29:37 +0000

    removed files (use `build.py`)

--- src/build/batch_build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-pyinstaller --noconfirm --log-level=WARN --clean \
-    --distpath "./bin/bin" --workpath "./bin/build" \
-    --name ROSA --onefile \
-    --paths "./.venv/Lib/site-packages" \
-    --add-binary "./src/ROSA/responses:./responses" \
-    --icon "./docs/ico/hotpot-ai.ico" \
-    ./src/ROSA/main.py
